6bedc453e85f9a8fe19382111b2e8ad8db0d484b
Irina <yaychenya2014@gmail.com>
2025-12-29 08:30:30 +0200

    feat: implement express server for notes

--- src/server.js
+++ src/server.js
@@ -35,8 +35,9 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+
 });
